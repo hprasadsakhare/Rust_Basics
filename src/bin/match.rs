1a30742395f094c7805ca392500b20d593212d61
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2023-08-21 00:03:29 +0530

    first commit

---
+++ src/bin/match.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let my_name = "jayson";
+    match my_name{
+        "jayson" => println!("this is my name"),
+        "bob" => println!("this is not my name"),
+        "allice" => println!("hello alice"),
+        _ => println!("nice to meet you"),
+
+    }
+}
