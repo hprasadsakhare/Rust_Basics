c2f1597dee108b229e93a593cc2ef0d938963a16
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2023-08-24 07:35:12 +0530

    first commit

---
+++ src/bin/impl1.rs
@@ -0,0 +1,13 @@
+struct Temperature{
+    degrees_f: f64,
+}
+
+impl Temperature {
+    fn show_temp(temp: Temperature){
+        println!("{:?} degrees F",temp.degrees_f);
+    }
+}
+fn main(){
+    let hot = Temperature {degrees_f: 99.9};
+    Temperature::show_temp(hot);
+}
