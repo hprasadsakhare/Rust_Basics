8891185b5295eaa8d2e08c8ff8519aa89bd9eaaf
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2023-08-25 07:30:38 +0530

    first commit

---
+++ src/bin/string1.rs
@@ -0,0 +1,20 @@
+struct LineItem{
+    name: String,
+    count: i32,
+}
+
+fn main(){
+    let receipt = vec![
+        LineItem{
+            name: "cereal".to_owned(),
+            count: 1,
+        },
+        LineItem{
+            name: String::from("fruit"),
+            count: 3,
+        },
+    ];
+    for item in receipt{
+        println!("name: {:?}, count: {:?}",item.name,item.count);
+    }   
+}
